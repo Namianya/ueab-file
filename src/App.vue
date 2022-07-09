6d215b98e9bb714f80eed972eb5b0ddd28a29762
Namianya <wekesa350@gmail.com>
2022-07-09 16:06:08 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+   
+    <div>
+
+      <navn class="flex w-full justify-between">
+        <!-- <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink> -->
+        <p>Hello</p>
+        <p>world</p>
+      </navn>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+
+
+
+</style>

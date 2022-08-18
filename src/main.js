d94f7a7678e3f3d2b0f63475ea83d71e6ca58b2f
nonniekaimburi <nonniekaimburi@gmail.com>
2022-08-18 23:34:59 +0300

    firebase auth
    Co-authored-by: Paul Wekesa <Namianya@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { initializeApp } from "firebase/app";
 
 import App from './App.vue'
 import router from './router'
 
 import './index.css'
+import {firebaseConfig} from "./firebase/init"
 
+initializeApp(firebaseConfig);
 const app = createApp(App)
 
 app.use(createPinia())

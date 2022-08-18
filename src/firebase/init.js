6bae20c7568cabb7ae2b7b42cd84c6232f77a2e7
Namianya <wekesa350@gmail.com>
2022-08-18 22:44:41 +0300

    adde firebase files

---
+++ src/firebase/init.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDjJNBybOF7g_zeslOezDYSg5czTlnbAS0",
+  authDomain: "ueabfms.firebaseapp.com",
+  projectId: "ueabfms",
+  storageBucket: "ueabfms.appspot.com",
+  messagingSenderId: "361434967495",
+  appId: "1:361434967495:web:f6423a04482ca828be3de2",
+  measurementId: "G-ZJMEHG1NKE"
+};
+
+const firebaseapp = initializeApp(firebaseConfig);
+export default {firebaseapp, firebaseConfig};

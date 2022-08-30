c11f65a95997500243eebb8f3589a482996eac4d
Namianya <wekesa350@gmail.com>
2022-08-30 05:00:13 +0300

    landing page

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex ">
+        <div class="w-full">
+            <ul>
+                <li v-for="school in schools" :key="school">
+                    <LandingButtons :buttonText="school" />
+                </li>
+            </ul>
+        </div>
+        <div class="w-full text-right">
+            <button class="btn btn-md btn-primary">+ Add File</button>
+        </div>
+    </div>
+</template>
+<script  setup lang="ts">
+import LandingButtons from '../components/LandingButtons.vue';
+const schools = [
+    'School of Business',
+    'School of Health Sciences',
+    'School of Nursing',
+    'School of Education, Humanities and Social Sciences',
+    'School of Science and Technology',
+];
+</script>
